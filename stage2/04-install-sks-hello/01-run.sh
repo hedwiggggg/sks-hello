0e90861a0cdae894357dce630fa2404f7905c293
Silas Rosenkranz <silas@rosenkra.nz>
2021-03-13 11:16:05 +0100

    refactor: tab to space

--- stage2/04-install-sks-hello/01-run.sh
+++ stage2/04-install-sks-hello/01-run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -e
 
-install -d															"${ROOTFS_DIR}/boot/sks-hello"
-install -m 777 files/_detected.wav			"${ROOTFS_DIR}/boot/sks-hello/"
-install -m 777 files/_ready.wav					"${ROOTFS_DIR}/boot/sks-hello/"
+install -d                              "${ROOTFS_DIR}/boot/sks-hello"
+install -m 777 files/_detected.wav      "${ROOTFS_DIR}/boot/sks-hello/"
+install -m 777 files/_ready.wav         "${ROOTFS_DIR}/boot/sks-hello/"
 
-install -d															"${ROOTFS_DIR}/home/sks/hello"
-install -m 777 files/main.py						"${ROOTFS_DIR}/home/sks/hello/"
-install -m 777 files/requirements.txt		"${ROOTFS_DIR}/home/sks/hello/"
-install -m 777 files/run-sks-hello.sh		"${ROOTFS_DIR}/home/sks/hello/"
+install -d                              "${ROOTFS_DIR}/home/sks/hello"
+install -m 777 files/main.py            "${ROOTFS_DIR}/home/sks/hello/"
+install -m 777 files/requirements.txt   "${ROOTFS_DIR}/home/sks/hello/"
+install -m 777 files/run-sks-hello.sh   "${ROOTFS_DIR}/home/sks/hello/"
 
-install -m 644 files/sks-hello.service	"${ROOTFS_DIR}/etc/systemd/system/"
+install -m 644 files/sks-hello.service  "${ROOTFS_DIR}/etc/systemd/system/"
 
 on_chroot << EOF
 sudo chmod +x /home/sks/hello/run-sks-hello.sh
